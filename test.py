c9e619fcad72d4cafadc8da6a524332cb2997be0
Vib766962 <vibhusiva98@gmail.com>
2020-12-10 15:54:21 +0530

    First

---
+++ test.py
@@ -0,0 +1,24 @@
+import unittest
+from main import add,subtract,multiply
+
+class TestClass(unittest.TestCase):
+	def test_add(self):
+	    x = 5
+	    y = 6
+	    expected = 11
+	    actual = add(x,y)
+	    self.assertEqual(expected, actual) 
+
+	def test_subtract(self):
+		x = 10
+		y = 5
+		expected = 5
+		actual = subtract(x,y)
+		self.assertEqual(expected, actual) 
+
+	def test_multiply(self):
+		x = 10
+		y = 5
+		expected = 50
+		actual = multiply(x,y)
+		self.assertEqual(expected, actual) 
