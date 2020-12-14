147bc3a9798991e0182560e7e003ad169f2f0975
Vib766962 <vibhusiva98@gmail.com>
2020-12-14 17:07:00 +0530

    Next

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 from main import add,subtract,multiply
-#comment tests okssssssssss
+#comment tests oksssssssssss
 class TestClass(unittest.TestCase):
 	def test_add(self):
 	    x = 5
